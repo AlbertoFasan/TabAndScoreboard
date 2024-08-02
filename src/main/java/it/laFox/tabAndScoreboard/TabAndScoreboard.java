52c769d091fe6be73980e6c94f2c2603d2f32d09
AlbertoFasan <alb.fasan@gmail.com>
2024-08-02 14:16:09 +0200

    Setup

---
+++ src/main/java/it/laFox/tabAndScoreboard/TabAndScoreboard.java
@@ -0,0 +1,17 @@
+package it.laFox.tabAndScoreboard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TabAndScoreboard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
